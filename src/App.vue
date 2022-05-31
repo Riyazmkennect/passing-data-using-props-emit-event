e03ed77f3c0e2602d7dc15e5a0df13e89b59d1bd
Riyazmkennect <106138842+Riyazmkennect@users.noreply.github.com>
2022-05-31 11:45:56 +0530

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+    <HelloWorld msg="Passing data"/>
+    <Paren/>
+    <Child name="alishaa"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import Paren from './components/Paren .vue'
+import Child from './components/ Child.vue'
+import Paren from './components/Paren.vue'
+import Child from './components/Child.vue'
+
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    Paren,
+    Child,
+    Paren,
+    Child
+}
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Child.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <h1>Child component</h1>
+        <h2>{{name}}</h2>
+    </div>
+</template>
+<script>
+export default {
+    name:"Child",
+    props:{
+        name
+    }
+}
+</script>
